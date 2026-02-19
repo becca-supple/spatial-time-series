e0a3f16c86977ce631b3f0a7f4441b5d82e31bf6
Rebecca Supple <118771336+becca-supple@users.noreply.github.com>
2026-02-19 16:22:50 +0000

    Loading and manipulation of necessary spatial data

---
+++ Simulations/Data manipulation.R
@@ -0,0 +1,25 @@
+#load necessary packages
+library(dplyr)
+library(gstat)
+library(countrycode)
+library(tidyr)
+library(terra)
+
+#load necessary data & build spatial objects
+contdird <- read.csv("Simulations/contdird.csv") |> 
+  filter(conttype == 1) |> 
+  filter(year == 2016)
+
+europe_boundaries <- vect("Simulations/Eurostat Shapefile")
+
+codes <- c("AUS", "BEL", "BUL", "CZR", "DEN", "EST", "FIN", "FRN", "GMY", 
+           "GRC", "HUN", "IRE", "ITA", "LAT", "LIT", "NTH", "NOR", "POL", "POR",
+           "SLV", "SPN", "SWD", "SWZ", "UKG")
+
+eur_boundaries_cropped <- crop(europe_boundaries, 
+                               ext(2500000, 6500000, 1250000, 5416754.9853))
+eur_boundaries_cropped$Country <- countrycode(unique(eur_boundaries_cropped$CNTR_ID),
+                                              origin = "eurostat",
+                                              destination = "cowc")
+eur_boundaries_filtered <- subset(eur_boundaries_cropped, 
+                                  eur_boundaries_cropped$Country %in% codes)
